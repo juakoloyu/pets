eda42161faae72cb31609f0cf7f3e1b9f11a0cf1
Joaquin Logiudice <juakoloyu@gmail.com>
2022-07-11 16:49:54 -0300

    [Add] Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   resources :animals
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
